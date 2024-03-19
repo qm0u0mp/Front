bd07293903dda0841a693a853f95519df87a2b17
WooHaNeul9 <99dngksmf@gmail.com>
2024-03-19 22:34:07 +0900

    자바 스크립트

---
+++ javascript/1.JS_Intro/remember.js
@@ -0,0 +1,10 @@
+// node.js 설치
+// cmd 창에서 버전 확인
+// vscode 터미널에서 npm install -g npm@latest 실행
+
+// 터미널 창에 파일 옮기기
+// cd 옮겨가고 싶은 파일 입력 후 엔터
+// cd 6.JS_Operator 파일로 이동하고 싶으면 cd 6 + tab + Enter
+
+// 콘솔 찍은 후 실행
+// node 파일이름
