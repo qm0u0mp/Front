a2d3c242fa0e261b94514b517ab7ecda9ea5e4cc
WooHaNeul9 <99dngksmf@gmail.com>
2024-03-19 23:39:04 +0900

    자바 스크립트 실행 설명서 작성

--- javascript/1.JS_Intro/remember.js
+++ javascript/1.JS_Intro/remember.js
@@ -1,13 +1,15 @@
-// node.js 설치
-// cmd 창에서 버전 확인
-// vscode 터미널에서 npm install -g npm@latest 실행
+//===== 자바 스크립트 실행하기 위한 셋팅=====//
 
-// 터미널 창에 파일 옮기기
-// cd 옮겨가고 싶은 파일 입력 후 엔터
-// cd 6.JS_Operator 파일로 이동하고 싶으면 cd 6 + tab + Enter
+// 1. node.js 설치
+// 2. cmd 창에서 버전 확인
+// 3. vscode 터미널에서 npm install -g npm@latest 실행
 
-// 콘솔 찍은 후 실행
+//===== 터미널 창에 파일 옮기기 =====//
+// cd 파일명 + Enter
+// Ex) 6.JS_Operator 파일로 이동하고 싶으면 =>  cd 6 + tab + Enter
+
+//===== console.log 실행 =====//
 // node 파일이름
 
-// 파일 뒤로 가기
+//===== 파일 뒤로 가기 =====//
 // cd ../
